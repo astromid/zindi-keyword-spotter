1e778ac96666f57f45946f04a3fd35340be8849d
Yan Budakyan <astromid@gmail.com>
2020-09-19 01:25:57 +0300

    feat(General-code): 1st implementation of dataset, simple melspec-cnn model

---
+++ src/dataset.py
@@ -0,0 +1,59 @@
+import torch
+import warnings
+from torch.utils.data import Dataset
+from tqdm.auto import tqdm
+from pathlib import Path
+from typing import Optional, Sequence
+from scipy.io import wavfile
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
+
+def pad_sample(sample: np.ndarray, pad_length: int) -> np.ndarray:
+    n = len(sample)
+    if n < pad_length:
+        return np.pad(sample, (pad_length - n, 0), constant_values=0)
+    else:
+        return sample[:pad_length]
+
+
+class ZindiAudioDataset(Dataset):
+
+    def __init__(
+        self,
+        pad_length: int,
+        sample_rate: int,
+        file_paths: Optional[Sequence[Path]] = None,
+        labels: Optional[Sequence[str]] = None,
+    ) -> None:
+        super().__init__()
+        self.pad_length = pad_length
+        self.sample_rate = sample_rate
+        self.file_paths = file_paths
+        self.labels = labels
+        self.le = None
+        self.label_idxs = None
+        
+        if self.labels is not None:
+            self.le = LabelEncoder()
+            self.le.fit(sorted(self.labels))
+            self.label_idxs = self.le.transform(self.labels)
+        
+        self.samples = []
+        for path in tqdm(self.file_paths, desc='Loading files'):
+            rate, sample = wavfile.read(path)
+            self.samples.append(sample)
+
+            if rate != self.sample_rate:
+                warnings.warn(f'File {path.name} has different sample rate {rate}')
+
+    def __len__(self) -> int:
+        return len(self.samples)
+    
+    def __getitem__(self, index: int):
+        sample = pad_sample(self.samples[index], self.pad_length)
+        sample_tensor = torch.from_numpy(sample)
+        if self.label_idxs is not None:
+            return sample_tensor, self.label_idxs[index]
+        else:
+            return sample_tensor
---
+++ src/models.py
@@ -0,0 +1,61 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import sampler
+from torchaudio.transforms import MelSpectrogram
+
+
+class PalSolModel(nn.Module):
+
+    def __init__(self, num_classes: int, sample_rate: int) -> None:
+        super().__init__()
+        self.melspectrogram = MelSpectrogram(sample_rate=sample_rate)
+        self.norm_input = nn.BatchNorm2d(num_features=1)
+        self.maxpool = nn.MaxPool2d(kernel_size=2)
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=2)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=48, kernel_size=3)
+        self.conv4 = nn.Conv2d(in_channels=48, out_channels=48, kernel_size=3)
+        self.conv5 = nn.Conv2d(in_channels=48, out_channels=96, kernel_size=3)
+        self.conv6 = nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3)
+        self.conv7 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3)
+
+        self.drop1 = nn.Dropout(p=0.1)
+        self.drop2 = nn.Dropout(p=0.2)
+
+        self.fc = nn.Linear(in_features=1536, out_features=256)
+        self.fc_norm = nn.BatchNorm1d(num_features=256)
+        self.linear = nn.Linear(in_features=256, out_features=num_classes)
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.melspectrogram(x)
+        x = x.unsqueeze(1)
+        x = self.norm_input(x)
+
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.maxpool(x)
+        x = self.drop1(x)
+
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.maxpool(x)
+        x = self.drop2(x)
+
+        x = F.relu(self.conv5(x))
+        x = self.maxpool(x)
+        x = self.drop2(x)
+
+        x = F.relu(self.conv6(x))
+        x = self.maxpool(x)
+        x = self.drop2(x)
+
+        x = F.relu(self.conv7(x))
+        x = self.maxpool(x)
+        x = self.drop2(x)
+
+        x = self.fc(x.flatten(start_dim=1))
+        x = self.fc_norm(x)
+        return self.linear(x)
+
